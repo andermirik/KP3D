399c4554b75655176d3b413f5b11d50e81ba8076
andermirik <33791601+andermirik@users.noreply.github.com>
2020-02-26 04:34:32 +0000

    init

---
+++ KP3D/MainWindow.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using Ara3D;
+using System.Diagnostics;
+
+namespace KP3D
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class MainWindow : Window
+    {
+        float fov = 180/2;
+        Scene.Scene scene;
+        Random rnd = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Scene.Scene.Camera camera = new Scene.Scene.Camera(new Vector3(0, 0, 100), new Vector3(0, 0, 0), fov);
+            scene = new Scene.Scene(camera);
+            Shapes.IcoSphere fig = new Shapes.IcoSphere(2);
+            fig.dx = 300;
+            fig.dy = 200;
+            fig.dz = 50;
+            fig.scale_x = 100;
+            fig.scale_y = 100;
+            fig.scale_z = 100;
+
+            Shapes.Box fig2 = new Shapes.Box();
+            fig2.dx = 100;
+            fig2.dy = 100;
+            fig2.dz = 0;
+            fig2.scale_x = 100;
+            fig2.scale_y = 100;
+            fig2.scale_z = 100;
+            //fig2.rotation_x = 3.14f / 3;
+            //scene.shapes.Add(fig);
+            scene.shapes.Add(fig2);
+
+        }
+
+        
+        public void render(int width, int height, System.Windows.Controls.Image image)
+        {
+            //image.Source = BitmapToImageSource(bm);
+            
+           
+            SceneView.Source = BitmapToImageSource(scene.render(580, 440));
+        }
+
+
+        private void SceneView_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SceneView.Source = BitmapToImageSource(scene.render(580, 440));
+        }
+
+        private void FOV_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            //render(400, 400, SceneView);
+        }
+
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+
+        private void SceneView_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (e.Delta > 0)
+                scene.camera.fov += 1f;
+
+            else if (e.Delta < 0)
+                scene.camera.fov -= 1f;
+            SceneView.Source = BitmapToImageSource(scene.render(580, 440));
+        }
+
+        int px = 0;
+        int py = 0;
+        private void SceneView_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                int x = (int)e.GetPosition(this).X;
+                int y = (int)e.GetPosition(this).Y;
+                if(px == 0 && py == 0)
+                {
+                    px = x;
+                    py = y;
+                }
+                int dx = x - px;
+                int dy = y - py;
+
+                dy = 0;
+
+                if (Math.Abs(dx) > 1 || Math.Abs(dy) > 1) {
+                    scene.camera.OnMouseMove(-dx, dy);
+                    scene.camera.gen_eye(100);
+                    SceneView.Source = BitmapToImageSource(scene.render(580, 440));
+                    Debug.WriteLine(scene.camera.latitude);
+                    Debug.WriteLine(scene.camera.longitude);
+                    Debug.WriteLine(scene.camera.center);
+                    px = x;
+                    py = y;
+                }
+            }
+            else if (e.RightButton == MouseButtonState.Pressed)
+            {
+                int x = (int)e.GetPosition(this).X;
+                int y = (int)e.GetPosition(this).Y;
+                if (px == 0 && py == 0)
+                {
+                    px = x;
+                    py = y;
+                }
+                int dx = x - px;
+                int dy = y - py;
+
+                dx = 0;
+
+                if (Math.Abs(dx) > 1 || Math.Abs(dy) > 1)
+                {
+                    scene.camera.OnMouseMove(-dx, -dy);
+                    scene.camera.gen_eye(100);
+                    SceneView.Source = BitmapToImageSource(scene.render(580, 440));
+                    Debug.WriteLine(scene.camera.latitude);
+                    Debug.WriteLine(scene.camera.longitude);
+                    Debug.WriteLine(scene.camera.center);
+                    px = x;
+                    py = y;
+                }
+            }
+            else
+            {
+                px = 0;
+                py = 0;
+            }
+        }
+
+        private void latitude_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            scene.camera.latitude = (float)latitude.Value;
+            scene.camera.gen_eye(100);
+            //scene.camera.gen_center(100);
+            SceneView.Source = BitmapToImageSource(scene.render(580, 440));
+        }
+
+        private void longitude_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            scene.camera.longitude = (float)longitude.Value;
+            scene.camera.gen_eye(100);
+            //scene.camera.gen_center(100);
+            SceneView.Source = BitmapToImageSource(scene.render(580, 440));
+        }
+    }
+}
---
+++ KP3D/MyMath/MyMatrix4x4.cs
@@ -0,0 +1,226 @@
+﻿using Ara3D;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KP3D.MyMath
+{
+    class MyMatrix4x4
+    {
+        public float[,] points = new float[4, 4];
+
+        public MyMatrix4x4()
+        {
+            points[0, 0] = 1;
+            points[1, 1] = 1;
+            points[2, 2] = 1;
+            points[3, 3] = 1;
+        }
+
+        public static MyMatrix4x4 Identity()
+        {
+            return new MyMatrix4x4();
+        }
+        public static MyMatrix4x4 CreateScale(float x, float y, float z)
+        {
+            var temp = new MyMatrix4x4();
+            temp.points[0, 0] = x;
+            temp.points[1, 1] = y;
+            temp.points[2, 2] = z;
+            return temp;
+        }
+
+        public static MyMatrix4x4 CreateTranslation(float dx, float dy, float dz)
+        {
+            MyMatrix4x4 temp = new MyMatrix4x4();
+            temp.points[0, 3] = dx;
+            temp.points[1, 3] = dy;
+            temp.points[2, 3] = dz;
+            return temp;
+        }
+
+        public static MyMatrix4x4 CreateRotationX(float A)
+        {
+            MyMatrix4x4 temp = new MyMatrix4x4();
+            temp.points[1, 1] = (float)Math.Cos(A);
+            temp.points[2, 1] = (float)Math.Sin(A);
+            temp.points[2, 2] = (float)Math.Cos(A);
+            temp.points[1, 2] = -(float)Math.Sin(A);
+            return temp;
+        }
+
+        public static MyMatrix4x4 CreateRotationY(float A)
+        {
+            MyMatrix4x4 temp = new MyMatrix4x4();
+            temp.points[0, 0] = (float)Math.Cos(A);
+            temp.points[0, 2] = -(float)Math.Sin(A);
+            temp.points[2, 0] = (float)Math.Sin(A);
+            temp.points[2, 2] = (float)Math.Cos(A);
+            return temp;
+        }
+
+        public static MyMatrix4x4 CreateRotationZ(float A)
+        {
+            MyMatrix4x4 temp = new MyMatrix4x4();
+            temp.points[0, 0] = (float)Math.Cos(A);
+            temp.points[0, 1] = -(float)Math.Sin(A);
+            temp.points[1, 0] = (float)Math.Sin(A);
+            temp.points[1, 1] = (float)Math.Cos(A);
+            return temp;
+        }
+
+        public static MyMatrix4x4 CreateLookAt(Vector3 eye, Vector3 center, Vector3 up)
+        {
+            Vector3 z = (eye - center).Normalize();
+            Vector3 x = up.Cross(z).Normalize();
+            Vector3 y = z.Cross(x).Normalize();
+            MyMatrix4x4 Minv = MyMatrix4x4.Identity();
+            MyMatrix4x4 Tr = MyMatrix4x4.Identity();
+
+            Minv.points[0, 0] = x.X;
+            Minv.points[1, 0] = y.X;
+            Minv.points[2, 0] = z.X;
+            Tr.points[0, 3] = -center.X;
+
+            Minv.points[0, 1] = x.Y;
+            Minv.points[1, 1] = y.Y;
+            Minv.points[2, 1] = z.Y;
+            Tr.points[1, 3] = -center.Y;
+
+            Minv.points[0, 2] = x.Z;
+            Minv.points[1, 2] = y.Z;
+            Minv.points[2, 2] = z.Z;
+            Tr.points[2, 3] = -center.Z;
+
+            return Minv * Tr;
+
+            //MyMatrix4x4 V = new MyMatrix4x4();
+            //Vector3 tmp = new Vector3(0, 1, 0);
+
+            //Vector3 forward = (eye - center).Normalize();
+            //Vector3 right = tmp.Normalize().Cross(forward);
+            //up = forward.Cross(right);
+
+            //V.points[0, 0] = right.X;
+            //V.points[0, 1] = right.Y;
+            //V.points[0, 2] = right.Z;
+            //V.points[1, 0] = up.X;
+            //V.points[1, 1] = up.Y;
+            //V.points[1, 2] = up.Z;
+            //V.points[2, 0] = forward.X;
+            //V.points[2, 1] = forward.Y;
+            //V.points[2, 2] = forward.Z;
+
+            //V.points[3, 0] = eye.X;
+            //V.points[3, 1] = eye.Y;
+            //V.points[3, 2] = eye.Z;
+
+            //return V;
+        }
+
+        public static MyMatrix4x4 CreateProjectionFOV(float angleOfView, float near, float far)
+        {
+            MyMatrix4x4 temp = new MyMatrix4x4();
+            float scale = 1 / (float)Math.Tan(angleOfView * 0.5 * Math.PI / 180);
+            temp.points[0, 0] = scale; // scale the x coordinates of the projected point 
+            temp.points[1, 1] = scale; // scale the y coordinates of the projected point 
+            temp.points[2, 2] = -far / (far - near); // used to remap z to [0,1] 
+            temp.points[3, 2] = -far * near / (far - near); // used to remap z [0,1] 
+            temp.points[2, 3] = -1; // set w = -z 
+            temp.points[3, 3] = 0;
+            return temp;
+        }
+
+        public static MyMatrix4x4 CreateViewPort(int x, int y, int w, int h, int depth)
+        {
+            MyMatrix4x4 m = MyMatrix4x4.Identity();
+            m.points[0, 3] = x + w / 2.0f;
+            m.points[1, 3] = y + h / 2.0f;
+            m.points[2, 3] = depth / 2.0f;
+
+            m.points[0, 0] = w / 2.0f;
+            m.points[1, 1] = h / 2.0f;
+            m.points[2, 2] = depth / 2.0f;
+            return m;
+        }
+
+        public static MyMatrix4x4 operator*(MyMatrix4x4 A, MyMatrix4x4 B)
+        {
+            MyMatrix4x4 C = new MyMatrix4x4();
+
+            for (var i = 0; i < 4; i++)
+            {
+                for (var j = 0; j < 4; j++)
+                {
+                    C.points[i, j] = 0;
+
+                    for (var k = 0; k < 4; k++)
+                    {
+                        C.points[i, j] += A.points[i, k] * B.points[k, j];
+                    }
+                }
+            }
+
+            return C;
+        }
+
+        public static MyMatrix4x4 FromPoint(Vector3 vector)
+        {
+            MyMatrix4x4 temp = new MyMatrix4x4();
+            temp.points[0, 0] = vector.X;
+            temp.points[1, 1] = vector.Y;
+            temp.points[2, 2] = vector.Z;
+            return temp;
+        }
+        
+        public static Vector3 ToPoint(MyMatrix4x4 m)
+        {
+           return new Vector3(m.points[0, 0], m.points[1, 1], m.points[2, 2]);
+        }
+    }
+    class MyMatrix4x1
+    {
+        public float[,] points = new float[4, 1];
+
+        public MyMatrix4x1()
+        {
+            points[0, 0] = 1;
+            points[1, 0] = 1;
+            points[2, 0] = 1;
+            points[3, 0] = 1;
+        }
+
+        public static MyMatrix4x1 operator *(MyMatrix4x4 A, MyMatrix4x1 B)
+        {
+            MyMatrix4x1 C = new MyMatrix4x1();
+
+            for (var i = 0; i < 4; i++)
+            {
+                for (var j = 0; j < 1; j++)
+                {
+                    C.points[i, j] = 0;
+
+                    for (var k = 0; k < 4; k++)
+                    {
+                        C.points[i, j] += A.points[i, k] * B.points[k, j];
+                    }
+                }
+            }
+
+            return C;
+        }
+
+        public static MyMatrix4x1 FromPoint4x1(Vector3 vector)
+        {
+            MyMatrix4x1 temp = new MyMatrix4x1();
+            temp.points[0, 0] = vector.X;
+            temp.points[1, 0] = vector.Y;
+            temp.points[2, 0] = vector.Z;
+            return temp;
+        }
+        public static Vector3 ToPoint(MyMatrix4x1 m)
+        {
+            return new Vector3(m.points[0, 0], m.points[1, 0], m.points[2, 0]);
+        }
+    }
+}
